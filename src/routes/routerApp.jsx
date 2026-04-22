ef3245feb1dae4384fc76f5ceaac2f4d6145a3ed
SantiagoZVcesde <zapatavillasantiago@gmail.com>
2026-04-21 20:03:56 -0500

    Chore:creacion de enrutado y instalacion de dependencias Agregando rutas de Gallery, Booking y Philosophy

--- src/routes/routerApp.jsx
+++ src/routes/routerApp.jsx
@@ -1,1 +1,23 @@
-a
+import App from '../App'
+import Gallery from '../pages/Gallery'
+import Booking from '../pages/Booking'
+import Philosophy from '../pages/Philosophy'
+
+export let routerApp = [
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "/gallery",
+        element: <Gallery />
+    },
+    {
+        path: "/booking",
+        element: <Booking />
+    },
+    {
+        path: "/philosophy",
+        element: <Philosophy />
+    }
+]
